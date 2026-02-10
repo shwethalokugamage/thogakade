2867d8885fb7207ec674aedfb8fccd20d76805e3
shwethalokugamage <shwethalokugamage@gmail.com>
2026-02-10 17:32:40 +0530

    add pom

---
+++ src/main/java/controller/customer/CustomerFormController.java
@@ -0,0 +1,74 @@
+package controller.customer;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CustomerFormController {
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerAddress;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerId;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerName;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerSalary;
+
+    @FXML
+    private TableView<?> tblCustomer;
+
+    @FXML
+    private TextField txtCustomerAddress;
+
+    @FXML
+    private TextField txtCustomerId;
+
+    @FXML
+    private TextField txtCustomerName;
+
+    @FXML
+    private TextField txtCustomerSalary;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
